9540bbc7286c3dac6bae559baaaa8cf7cefca90b
bhavinsimform <bhavin.n@simformsolutions.com>
2023-04-07 16:24:21 +0530

    Added comment on function

--- src/HomeController.php
+++ src/HomeController.php
@@ -4,11 +4,17 @@ namespace OpenSource\AutomationOpenai;
 
 class HomeController
 {
+    /**
+     * This function is for adding two values
+     */
     public function add($a, $b)
     {
         return $a + $b;
     }
 
+    /**
+     * This function is for substraction of two values
+     */
     public function sub($a, $b)
     {
         return $a - $b;
