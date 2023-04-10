40fafc28f9fe5d97689386da53f5265a7f807b81
bhavinsimform <bhavin.n@simformsolutions.com>
2023-04-10 16:03:19 +0530

    Implement env loader

--- src/index.php
+++ src/index.php
@@ -1,8 +1,20 @@
 <?php
 namespace OpenSource\AutomationOpenai;
 
+require_once ('../vendor/autoload.php');
+
 require_once("Api/Webhook.php");
 
 use OpenSource\AutomationOpenai\Api\Webhook;
+use Symfony\Component\Dotenv\Dotenv;
+
+$dotenv = new Dotenv();
+$dotenv->load('../.env');
+
+// overwrites existing env variables
+$dotenv->overload('../.env');
+
+// loads .env, .env.local, and .env.$APP_ENV.local or .env.$APP_ENV
+$dotenv->loadEnv('../.env');
 
 $webhook = new Webhook();
