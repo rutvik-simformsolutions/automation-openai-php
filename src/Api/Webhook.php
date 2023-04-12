6347960b4c644faa09846b6cacdc7e28fcc1d7fe
mritunjay khichi <mritunjay.k@simformsolutions.com>
2023-04-12 14:43:50 +0530

    Create Confluence Page using Rest API

--- src/Api/Webhook.php
+++ src/Api/Webhook.php
@@ -3,16 +3,22 @@
 namespace OpenSource\AutomationOpenai\Api;
 require_once("Services/ChatGPTService.php");
 require_once("Services/GitService.php");
+require_once("Services/BitbucketService.php");
 
 use OpenSource\AutomationOpenai\Services\ChatGPTService;
+use OpenSource\AutomationOpenai\Services\BitbucketService;
 
 class Webhook 
 {
     public function __construct()
     {
+        $pageId = $_ENV['JIRA_PAGE'];
         $ChatGPTService = new ChatGPTService();
-        shell_exec('./script.sh'); // execute git log script file - not working on local machine
-        $recentCommits = addslashes(json_encode("write change log documentation from below points: \n ".file_get_contents("output.txt")));
-        print_r($ChatGPTService->openaiChat($recentCommits));
+        $BitbucketService = new BitbucketService();
+        shell_exec('./script.sh');
+        $recentCommits = addslashes(json_encode("write change log documentation from this points. \n ".file_get_contents("output.txt")));
+        $openAI = $ChatGPTService->openaiChat($recentCommits);
+        $jiraPage = json_decode($BitbucketService->viewPage($pageId));
+        echo $confluencePage = $BitbucketService->updatePage($pageId,$jiraPage->title,$jiraPage->version->number,$openAI);
     }
 }
