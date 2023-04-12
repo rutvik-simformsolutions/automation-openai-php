124e01a1488f5c99fe1a00e941d3ee0a70aa8071
mritunjay khichi <mritunjay.k@simformsolutions.com>
2023-04-12 16:58:05 +0530

    Create change log table for confluence page.

--- src/Api/Webhook.php
+++ src/Api/Webhook.php
@@ -12,13 +12,18 @@ class Webhook
 {
     public function __construct()
     {
-        $pageId = $_ENV['JIRA_PAGE'];
+        $pageId = $_ENV['BITBUCKET_PAGE'];
         $ChatGPTService = new ChatGPTService();
         $BitbucketService = new BitbucketService();
         shell_exec('./script.sh');
-        $recentCommits = addslashes(json_encode("write change log documentation from this points. \n ".file_get_contents("output.txt")));
+        $recentCommits = "write change log documentation from this points. \n ".file_get_contents("output.txt")." before providing output please consider the points for formatting, first do not include any heading or title, secondly use && as number for each point, third the number of output points should be equal to number of points provided to you and last every point should be explained in descriptive manner and there should be single line break after each point.";
         $openAI = $ChatGPTService->openaiChat($recentCommits);
-        $jiraPage = json_decode($BitbucketService->viewPage($pageId));
-        echo $confluencePage = $BitbucketService->updatePage($pageId,$jiraPage->title,$jiraPage->version->number,$openAI);
+        $changeLog = array_filter(explode('&&', $openAI), fn($value) => !is_null($value) && $value !== '');
+        $html = "<table border='1'> <tbody> <tr> <th class='confluenceTh'>Task</th> <th class='confluenceTh'> Status </th> </tr>";
+        foreach ($changeLog as $log) {
+            $html .= "<tr> <td class='confluenceTd'>$log</td> <td class='confluenceTd'> &#10004; </td> </tr>";
+        }
+        $html .= "</tbody> </table>";
+        echo $BitbucketService::generateConfluencePage($html,'OpenAI Change Logs '.date('d/m/y H:i'));
     }
 }
--- src/Services/ChatGPTService.php
+++ src/Services/ChatGPTService.php
@@ -3,35 +3,47 @@ namespace OpenSource\AutomationOpenai\Services;
 
 class ChatGPTService
 {
-    public function openaiChat($prompt){
+	public static function openaiChat(String $prompt)
+    {
+        try {
 
-		$curl = curl_init();
-		curl_setopt_array($curl, array(
-		  CURLOPT_URL => "https://api.openai.com/v1/chat/completions",
-		  CURLOPT_RETURNTRANSFER => true,
-		  CURLOPT_ENCODING => "",
-		  CURLOPT_MAXREDIRS => 10,
-		  CURLOPT_TIMEOUT => 30,
-		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		  CURLOPT_CUSTOMREQUEST => "POST",
-		  CURLOPT_POSTFIELDS =>'{
-		  "model": "gpt-3.5-turbo",
-		  "messages": [{"role": "user", "content": " '.$prompt.' "}]
-		  }',
-		  CURLOPT_HTTPHEADER => array(
-		    "Content-Type: application/json",
-		    "Authorization: Bearer " . $_ENV['API_KEY']
-		  ),
-		));
-	    $response = curl_exec($curl);
-	    $err = curl_error($curl);
-	    curl_close($curl);
-	    if ($err) {
-	        return "cURL Error #:" . $err;
-	    } else {
-	        $response_json = json_decode($response, true);
-	        $doc_text = $response_json['choices'][0]['message']['content'];
-	        return $doc_text;
-	    }
+            if (empty($prompt))
+                return "empty prompt!";
+
+            if (!isset($_ENV['API_KEY']) || empty($_ENV['API_KEY']))
+                return "Error : openaiKey not available!";
+
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, 'https://api.openai.com/v1/chat/completions');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_POST, 1);
+
+            $headers = [];
+            $headers[] = 'Content-Type: application/json';
+            $headers[] = "Authorization: Bearer " . $_ENV['API_KEY'];
+            $data = [
+                "model" => "gpt-3.5-turbo",
+                "messages" => array(["role" => "user", "content" => $prompt])
+            ];
+            $jsonData = json_encode($data);
+
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+
+            $result = curl_exec($ch);
+            $err = curl_error($ch);
+            curl_close($ch);
+
+            if ($err) {
+                throw new \Exception("Curl Error :" . $err);
+            } else {
+                $response = json_decode($result, true);
+	        	$output = $response['choices'][0]['message']['content'];
+	        	return $output;
+            }
+
+        } catch (\Exception $exception) {
+            return $exception->getMessage();
+        }
     }
 }
