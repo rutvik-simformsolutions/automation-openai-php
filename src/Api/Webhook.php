4ddb59fdf6d0135ab7144233917a3ced15feff8b
bhavinsimform <bhavin.n@simformsolutions.com>
2023-04-13 11:29:14 +0530

    Set env variable in github

--- src/Api/Webhook.php
+++ src/Api/Webhook.php
@@ -12,7 +12,8 @@ class Webhook
 {
     public function __construct()
     {
-        $pageId = $_ENV['BITBUCKET_PAGE'];
+        echo $_ENV['PROJECT_NAME'];
+        /*$pageId = $_ENV['BITBUCKET_PAGE'];
         $ChatGPTService = new ChatGPTService();
         $BitbucketService = new BitbucketService();
         shell_exec('./script.sh');
@@ -24,6 +25,6 @@ class Webhook
             $html .= "<tr> <td class='confluenceTd'>$log</td> <td class='confluenceTd'> &#10004; </td> </tr>";
         }
         $html .= "</tbody> </table>";
-        echo $BitbucketService::generateConfluencePage($html,'OpenAI Change Logs '.date('d/m/y H:i'));
+        echo $BitbucketService::generateConfluencePage($html,'OpenAI Change Logs '.date('d/m/y H:i'));*/
     }
 }
