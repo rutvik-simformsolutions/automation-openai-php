80144f90abcc9a3c958eedf0bfc31ea2d48c29c9
bhavinsimform <bhavin.n@simformsolutions.com>
2023-04-14 12:02:50 +0530

    Fix array issue

--- src/Api/Webhook.php
+++ src/Api/Webhook.php
@@ -25,21 +25,28 @@ class Webhook
             shell_exec('./script.sh');
             //$recentCommits = "Write change log documentation from this points. \n ".file_get_contents("output.txt")." before providing output please consider the points for formatting, first do not include any heading or title, secondly use && as number for each point, third the number of output points should be equal to number of points provided to you and last every point should be explained in descriptive manner and there should be single line break after each point.";
             $recentCommits = "Write change log documentation from this points. \n ".file_get_contents("output.txt");
-            print_r($recentCommits);
-            echo "---------------1---------------------";
+            //print_r($recentCommits);
+            //echo "---------------1---------------------";
 
             $openAI = $chatGPTService->openaiChat($recentCommits);
-            print_r($openAI);
-            echo "-----------------2-------------------";
-            $changeLog = array_filter(explode('&&', $openAI), fn($value) => !is_null($value) && $value !== '');
-            print_r($changeLog);
-            echo "-----------------3-------------------";
+            //print_r($openAI);
+            //echo "-----------------2-------------------";
+            //$changeLog = array_filter(explode('&&', $openAI), fn($value) => !is_null($value) && $value !== '');
+            //print_r($changeLog);
+            //echo "-----------------3-------------------";
             /*$html = "<table border='1'> <tbody> <tr> <th class='confluenceTh'>Task</th> <th class='confluenceTh'> Status </th> </tr>";
             foreach ($changeLog as $log) {
                 $html .= "<tr> <td class='confluenceTd'>$log</td> <td class='confluenceTd'> &#10004; </td> </tr>";
             }
             $html .= "</tbody> </table>";*/
-            $html = $changeLog;
+            if (is_array($openAI)) {
+                foreach($openAI as $log) {
+                    $html .= $log;
+                }
+            } else {
+                $html = $openAI;
+            }
+            
             $bitbucketService::generateConfluencePage($html,'OpenAI Change Logs '.date('d/m/y H:i'));
         }
     }
