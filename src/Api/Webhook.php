42d17f57f90d003590a7e98340cca690a3180f3e
bhavinsimform <bhavin.n@simformsolutions.com>
2023-04-14 11:41:32 +0530

    testing changelog html

--- src/Api/Webhook.php
+++ src/Api/Webhook.php
@@ -26,6 +26,7 @@ class Webhook
             //$recentCommits = "Write change log documentation from this points. \n ".file_get_contents("output.txt")." before providing output please consider the points for formatting, first do not include any heading or title, secondly use && as number for each point, third the number of output points should be equal to number of points provided to you and last every point should be explained in descriptive manner and there should be single line break after each point.";
             $recentCommits = "Write change log documentation from this points. \n ".file_get_contents("output.txt");
             $openAI = $chatGPTService->openaiChat($recentCommits);
+            print_r($openAI);
             $changeLog = array_filter(explode('&&', $openAI), fn($value) => !is_null($value) && $value !== '');
             /*$html = "<table border='1'> <tbody> <tr> <th class='confluenceTh'>Task</th> <th class='confluenceTh'> Status </th> </tr>";
             foreach ($changeLog as $log) {
@@ -33,7 +34,7 @@ class Webhook
             }
             $html .= "</tbody> </table>";*/
             $html = $changeLog;
-            echo $bitbucketService::generateConfluencePage($html,'OpenAI Change Logs '.date('d/m/y H:i'));
+            $bitbucketService::generateConfluencePage($html,'OpenAI Change Logs '.date('d/m/y H:i'));
         }
     }
 }
