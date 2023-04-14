ebe2046e2fb9c76bfddee5d9287568f5226a19a1
bhavinsimform <bhavin.n@simformsolutions.com>
2023-04-14 11:52:32 +0530

    Print output of object

--- src/Api/Webhook.php
+++ src/Api/Webhook.php
@@ -25,9 +25,12 @@ class Webhook
             shell_exec('./script.sh');
             //$recentCommits = "Write change log documentation from this points. \n ".file_get_contents("output.txt")." before providing output please consider the points for formatting, first do not include any heading or title, secondly use && as number for each point, third the number of output points should be equal to number of points provided to you and last every point should be explained in descriptive manner and there should be single line break after each point.";
             $recentCommits = "Write change log documentation from this points. \n ".file_get_contents("output.txt");
+            print_r($recentCommits);
+
             $openAI = $chatGPTService->openaiChat($recentCommits);
             print_r($openAI);
             $changeLog = array_filter(explode('&&', $openAI), fn($value) => !is_null($value) && $value !== '');
+            print_r($changeLog);
             /*$html = "<table border='1'> <tbody> <tr> <th class='confluenceTh'>Task</th> <th class='confluenceTh'> Status </th> </tr>";
             foreach ($changeLog as $log) {
                 $html .= "<tr> <td class='confluenceTd'>$log</td> <td class='confluenceTd'> &#10004; </td> </tr>";
