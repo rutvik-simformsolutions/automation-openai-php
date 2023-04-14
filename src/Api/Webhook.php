3567acbea38e96e5436496278f522128b2b35cf1
bhavinsimform <bhavin.n@simformsolutions.com>
2023-04-14 11:22:43 +0530

    Format AI output

--- src/Api/Webhook.php
+++ src/Api/Webhook.php
@@ -26,13 +26,13 @@ class Webhook
             //$recentCommits = "Write change log documentation from this points. \n ".file_get_contents("output.txt")." before providing output please consider the points for formatting, first do not include any heading or title, secondly use && as number for each point, third the number of output points should be equal to number of points provided to you and last every point should be explained in descriptive manner and there should be single line break after each point.";
             $recentCommits = "Write change log documentation from this points. \n ".file_get_contents("output.txt");
             $openAI = $chatGPTService->openaiChat($recentCommits);
-            $html = $openAI;
-            /*$changeLog = array_filter(explode('&&', $openAI), fn($value) => !is_null($value) && $value !== '');
-            $html = "<table border='1'> <tbody> <tr> <th class='confluenceTh'>Task</th> <th class='confluenceTh'> Status </th> </tr>";
+            $changeLog = array_filter(explode('&&', $openAI), fn($value) => !is_null($value) && $value !== '');
+            /*$html = "<table border='1'> <tbody> <tr> <th class='confluenceTh'>Task</th> <th class='confluenceTh'> Status </th> </tr>";
             foreach ($changeLog as $log) {
                 $html .= "<tr> <td class='confluenceTd'>$log</td> <td class='confluenceTd'> &#10004; </td> </tr>";
             }
             $html .= "</tbody> </table>";*/
+            $html = $changeLog;
             echo $bitbucketService::generateConfluencePage($html,'OpenAI Change Logs '.date('d/m/y H:i'));
         }
     }
