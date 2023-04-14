92671e731fb1c80db44030d1044d4175a1c4da33
bhavinsimform <bhavin.n@simformsolutions.com>
2023-04-14 16:53:38 +0530

    AI-2 Make private function for generate PCR

--- src/Api/Webhook.php
+++ src/Api/Webhook.php
@@ -25,42 +25,49 @@ class Webhook
             $openAI = $chatGPTService->openaiChat($recentCommits);
             $bitbucketService::generateConfluencePage($openAI, 'OpenAI Change Logs ' . date('d/m/y H:i'));
 
-            $pattern = '/AI-\d+/';
-            $taskIds = [];
-            preg_match_all($pattern, file_get_contents("output.txt"), $matches);
-            if (!empty($matches[0])) {
-                $taskIds = $matches[0];
-            }
-
-            $tasks = json_decode($bitbucketService::searchJiraTasks("AI"));
-            $taskStatus = [];
-            foreach ($tasks->issues as $task) {
-                $taskStatus[] = [
-                    'key' => $task->key,
-                    'status' => $task->fields->status->name,
-                    'title' => $task->fields->summary,
-                    'assignee' => @$task->fields->assignee->displayName,
-                ];
-            }
-            $html = "<table border='1'>
-                     <tbody>
-                        <tr>
-                            <th class='confluenceTh'> Task </th>
-                            <th class='confluenceTh'> Developer </th>
-                            <th class='confluenceTh'> Status </th>
-                            <th class='confluenceTh'> PR </th>
-                        </tr>";
-            foreach ($taskStatus as $task) {
-                $status = (in_array($task['key'], $taskIds)) ? "&#10004; pushed to production" : "";
-                $html .= "<tr>
-                            <td class='confluenceTd'> <a href='".$_ENV['TASK_URL']."/$task[key]' target='_blank'> $task[key] $task[title] </a> </td>
-                            <td class='confluenceTd'> $task[assignee] </td>
-                            <td class='confluenceTd'> $status </td>
-                            <td class='confluenceTd'>  </td>
-                          </tr>";
-            }
-            $html .= "</tbody> </table>";
-            $bitbucketService::generateConfluencePage($html, 'OpenAI PCR ' . date('d/m/y H:i'));
+            $htmlPCR = $this->getPcrHtml($bitbucketService);
+            $bitbucketService::generateConfluencePage($htmlPCR, 'OpenAI PCR ' . date('d/m/y H:i'));
         }
+    }
+
+    private function getPcrHtml($bitbucketService)
+    {
+        $pattern = '/AI-\d+/';
+        $taskIds = [];
+        preg_match_all($pattern, file_get_contents("output.txt"), $matches);
+        if (!empty($matches[0])) {
+            $taskIds = $matches[0];
+        }
+
+        $tasks = json_decode($bitbucketService::searchJiraTasks("AI"));
+        $taskStatus = [];
+        foreach ($tasks->issues as $task) {
+            $taskStatus[] = [
+                'key' => $task->key,
+                'status' => $task->fields->status->name,
+                'title' => $task->fields->summary,
+                'assignee' => @$task->fields->assignee->displayName,
+            ];
+        }
+        $html = "<table border='1'>
+                 <tbody>
+                    <tr>
+                        <th class='confluenceTh'> Task </th>
+                        <th class='confluenceTh'> Developer </th>
+                        <th class='confluenceTh'> Status </th>
+                        <th class='confluenceTh'> PR </th>
+                    </tr>";
+        foreach ($taskStatus as $task) {
+            $status = (in_array($task['key'], $taskIds)) ? "&#10004; pushed to production" : "";
+            $html .= "<tr>
+                        <td class='confluenceTd'> <a href='".$_ENV['TASK_URL']."/$task[key]' target='_blank'> $task[key] $task[title] </a> </td>
+                        <td class='confluenceTd'> $task[assignee] </td>
+                        <td class='confluenceTd'> $status </td>
+                        <td class='confluenceTd'>  </td>
+                      </tr>";
+        }
+        $html .= "</tbody> </table>";
+
+        return $html;
     }
 }
